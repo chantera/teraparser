b1ef61205a0b7bccd6dd47bc30c68bd97b1dc926
h.teranishi <teranishihiroki@gmail.com>
2016-07-23 19:14:15 +0900

    minor fix.

--- src/jp/naist/cl/srparser/app/Config.java
+++ src/jp/naist/cl/srparser/app/Config.java
@@ -6,7 +6,6 @@ import jp.naist.cl.srparser.util.DateUtils;
 import jp.naist.cl.srparser.util.FileUtils;
 import jp.naist.cl.srparser.util.StringUtils;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
