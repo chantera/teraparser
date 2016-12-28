9cf051948f50fe3f1308cb7be49e08767951ddf7
Hiroki Teranishi <teranishihiroki@gmail.com>
2016-12-28 23:29:17 +0900

    typo.

--- src/jp/teraparser/app/Evaluator.java
+++ src/jp/teraparser/app/Evaluator.java
@@ -14,18 +14,18 @@ public class Evaluator {
 
     public static double calcUAS(Map<Sentence.ID, Arc[]> goldArcMap, Map<Sentence.ID, Arc[]> predictArcMap) {
         double total = 0;
-        double collect = 0;
+        double correct = 0;
         for (Map.Entry<Sentence.ID, Arc[]> entry : goldArcMap.entrySet()) {
             Arc[] goldArcs = entry.getValue();
             Arc[] predictArcs = predictArcMap.get(entry.getKey());
             // i = 0 is null since <ROOT> has no arc.
             for (int i = 1; i < goldArcs.length; i++) {
                 if (goldArcs[i].equals(predictArcs[i])) {
-                    collect++;
+                    correct++;
                 }
                 total++;
             }
         }
-        return collect / total;
+        return correct / total;
     }
 }
